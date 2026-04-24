f71797b6638d850e461b5b6d0b00f8d43bbb7d14
Andrew Chen <ache337@aucklanduni.ac.nz>
2026-04-24 13:13:23 +1200

    fix: new email added

--- src/app/components/common/Footer.tsx
+++ src/app/components/common/Footer.tsx
@@ -65,11 +65,11 @@ export function Footer() {
                 +64 22 453 0098
               </a>
               <a
-                href="mailto:receptionauck@rentmyhome.co.nz"
+                href="mailto:info@rentmyhome.co.nz"
                 className="flex items-center gap-3 text-white text-sm hover:underline transition-colors"
               >
                 <Mail className="h-4 w-4" />
-                receptionauck@rentmyhome.co.nz
+                info@rentmyhome.co.nz
               </a>
             </div>
 
